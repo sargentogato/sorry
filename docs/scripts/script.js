731ed1d53b70c6e6d37b6bddef2cc6a4cc8f74e6
Sargentogato <marlonjvelasquez@gmail.com>
2024-02-21 18:04:29 +0100

    clean comments

--- docs/scripts/script.js
+++ docs/scripts/script.js
@@ -4,26 +4,18 @@ let containerMessage = document.querySelector(".message");
 let boton_no = document.getElementById("botonNo");
 
 const clickManagerBtnSi = (Event) => {
-  /* ******************** */
-  // Asegúrate de que la biblioteca Lottie esté cargada antes de ejecutar este código
-
-  // Obtén el contenedor donde deseas mostrar la animación
   var contenedor = document.getElementById("miAnimacion");
 
-  // Configura las opciones de la animación (ruta al archivo JSON, loop, autoplay, etc.)
   var opcionesAnimacion = {
     container: contenedor,
-    renderer: "svg", // o 'canvas' si prefieres
+    renderer: "svg",
     loop: true,
     autoplay: true,
-    path: "assets/imges/Animation.json", // Reemplaza esto con la ruta correcta a tu archivo JSON
+    path: "assets/imges/Animation.json",
   };
 
-  // Crea la instancia de la animación con las opciones
   var animacion = lottie.loadAnimation(opcionesAnimacion);
-  /* ******************** */
 
-  console.log("SI", Event);
   if (te_quiero_respuesta.classList.contains("hide")) {
     te_quiero_respuesta.classList.remove("hide");
   }
@@ -43,6 +35,5 @@ const clickManagerBtnNo = (Event) => {
   boton_no.style.top = Math.random() * 200 + "px";
 };
 
-// Muestra la respuesta de "Yo también te quiero" y oculta los botones
 boton_si.addEventListener("click", clickManagerBtnSi, false);
 boton_no.addEventListener("mouseover", clickManagerBtnNo);
